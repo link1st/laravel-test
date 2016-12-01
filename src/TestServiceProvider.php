73f37f9fb4bc78e61c94a440d5b5276a27ba8337
link <link@hongjitech.cn>
2016-12-01 17:18:43 +0800

    update

--- src/TestServiceProvider.php
+++ src/TestServiceProvider.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class LaravelTestServiceProvider extends ServiceProvider {
+class TestServiceProvider extends ServiceProvider {
 
 	/**
 	 * Indicates if loading of the provider is deferred.
