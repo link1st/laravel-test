87c973df90ec0d7dab1d75d11236b4995e2462a2
link <link@hongjitech.cn>
2016-12-01 16:50:50 +0800

    update

--- src/easemob.php
+++ src/easemob.php
@@ -5,9 +5,21 @@ use Config ;
 class easemob
 {
 
+    /**
+     * 获取配置项
+     * @return int
+     */
     function get_config(){
-        return 1;
+        return Config::get('EASEMOB_DOMAIN',"空");
     }
 
+
+    /**
+     * 默认
+     * @return mixed
+     */
+    public function index(){
+        return 'index';
+    }
 
 };
